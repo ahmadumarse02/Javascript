fe781a7999f31927e3b2e4f0c29d59fbe3b03355
ahmadumarse02 <ahmadumarse02@gmail.com>
2025-02-10 22:56:36 +0500

    math objects

---
+++ number/mathObject.js
@@ -0,0 +1,25 @@
+// Math Object
+// Math.floor(x) returns the value of x rounded down to its nearest integer
+console.log(Math.floor(-4.5))
+console.log(Math.floor(5.9999))
+console.log(Math.floor(5.5))
+
+// Math.round(x) return the nearest integer
+console.log(Math.floor(5.1))
+console.log(Math.floor(-5.199999))
+console.log(Math.floor(5.6))
+
+// Math.ceil(x) return the value of x rounded up to its nearest integer.
+console.log(Math.ceil(5.1))
+console.log(Math.ceil(-5.199999))
+console.log(Math.ceil(5.99))
+
+//Math.rendom() return the numer etween the 0 to 1.
+
+console.log(Math.random() + 1)
+
+console.log(Math.round(Math.random() * 20))
+
+// formula 
+// Math.random() * (max - min + 1) + min
+console.log(Math.floor(Math.random()*(22 - 10 + 1)) + 10)
